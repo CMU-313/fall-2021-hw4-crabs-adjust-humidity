b187997d0afdd2011c7e23025979d8d252a2442d
SantianLin <yanyulin@andrew.cmu.edu>
2021-11-02 18:15:00 -0400

    Create test_predict_model.py

--- dockerfile/apps/tests/test_predict_model.py
+++ dockerfile/apps/tests/test_predict_model.py
@@ -5,7 +5,43 @@ from apps import app
 def client():
     return app.app.test_client()
 
+#test basic functionality -- how predicted result match labels
 def test_predict_api(client):
-    r = client.get("/predict?age=18&health=5&absences=2")
+    r = client.get("/predict?Medu=4&failures=0&schoolsup=1&Dalc=1&Walc=1")
+    assert r.status_code == 200     #correctly returns
+    assert str(r.json) == '0'       #check if the result is corret
+    r = client.get("/predict?Medu=1&failures=3&schoolsup=0&Dalc=1&Walc=1")
     assert r.status_code == 200
-    assert str(r.json) == '1'
+    assert str(r.json) == '0'
+    r = client.get("/predict?Medu=2&failures=4&schoolsup=0&Dalc=1&Walc=1")
+    assert r.status_code == 200
+    assert str(r.json) == '0'
+    r = client.get("/predict?Medu=4&failures=4&schoolsup=0&Dalc=1&Walc=1")
+    assert r.status_code == 200
+    assert str(r.json) == '0'
+
+#test for accepting and extracting additional arguements
+def test_additional_arg(client):
+    r = client.get("/predict?Medu=4&failures=0&schoolsup=1&Dalc=1&Walc=1&sex=F&age=70")
+    assert r.status_code == 200
+    assert str(r.json) == '0'
+    r = client.get("/predict?Medu=4&failures=0&schoolsup=1&Dalc=1&Walc=1&sex=F&age=-14")
+    assert r.status_code == 200
+    assert str(r.json) == '0'
+    r = client.get("/predict?Medu=4&schoolsup=1&Dalc=1&Walc=1")
+    assert r.status_code == 400     #missing arguments will result in Bad Request error with code 400
+
+#test for error input type (string inputs)
+def test_str_type(client):
+    r = client.get("/predict?Medu=4&failures=0&schoolsup=1&Dalc=1&Walc=1&sex=F&age=-14")
+    assert r.status_code == 200     #A string input, which can be parsed to an integer will be normal
+    assert str(r.json) == '0'
+    r = client.get("/predict?Medu=4&failures=0&schoolsup=true&Dalc=1&Walc=1&sex=F&age=70")
+    assert r.status_code == 400     #A string input, which can't be parsed to an integer will have a response with code 400(Bad Request)
+
+#test for out of bound input type
+def test_oof(client):
+    r = client.get("/predict?Medu=6&failures=0&schoolsup=true&Dalc=1&Walc=1&sex=F&age=70")
+    assert r.status_code == 400     #out of bound input will have a response with code 400(Bad Request)
+    r = client.get("/predict?Medu=5&failures=0&schoolsup=true&Dalc=-1&Walc=1&sex=F&age=70")
+    assert r.status_code == 400     #out of bound input will have a response with code 400(Bad Request)
